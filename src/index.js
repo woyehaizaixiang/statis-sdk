98549bc61e5c0110e6c3c74fe704756bbf0ecf65
Kuangsong <519089437@qq.com>
2021-11-18 15:40:00 +0800

    v1.0.0

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// 错误日志上报fn
+function reportError(log){
+  let ua = window.navigator.userAgent;
+  let href = window.location.href;
+  new Image().src = `http://101.132.236.74:3000/log?log=${log}&ua=${ua}&host=${href}`;
+}
+
+function init(Vue){
+  window.addEventListener('error', error => {
+    reportError(error.message);
+  })
+
+  if(typeof(Vue) != 'undefined'){
+    Vue.config.errorHandler = function (error) {
+      reportError(error);
+    }
+  }
+}
+
+export default {
+  init,
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, './src/index.js')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      }
+    ]
+  },
+  mode: 'production',
+}
